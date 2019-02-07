398a9b7eb57b66c694d3df3686ad0d36a73e7244
Mayke Rezende <mayke.rezende@amcom.com.br>
2019-02-07 10:53:41 -0200

    Criação de scripts de virada de ano de 2018 para 2019

---
+++ scripts_virada_do_ano_2018_2019/01 - Cria novos cursos - 2019.sql
@@ -0,0 +1,129 @@
+use [GestaoPedagogica]
+go
+
+SET XACT_ABORT OFF
+BEGIN TRAN --rollback
+
+SET IDENTITY_INSERT ACA_Curso ON 
+insert into ACA_Curso
+       (cur_id, ent_id, cur_padrao, tne_id, tme_id, cur_codigo, cur_nome, cur_nome_abreviado,
+        cur_exclusivoDeficiente, cur_vigenciaInicio, cur_efetivacaoSemestral,
+        cur_situacao, cur_dataCriacao, cur_dataAlteracao)
+select cur_id + 20 as cur_id , ent_id, cur_padrao, tne_id, tme_id, cur_codigo, 
+	   replace(cur_nome,'2018','2019'), cur_nome_abreviado, cur_exclusivoDeficiente, 
+	   '2019-01-01' as cur_vigenciaInicio, cur_efetivacaoSemestral, cur_situacao, GETDATE(), GETDATE()
+  from ACA_Curso
+ where cur_nome like '2018%'
+
+if @@ERROR <> 0
+begin
+   PRINT 'Erro na criacao dos cursos'
+   ROLLBACK
+   SET IDENTITY_INSERT ACA_Curso OFF
+   RETURN
+end
+
+SET IDENTITY_INSERT ACA_Curso OFF;
+
+DISABLE TRIGGER TRG_ACA_Curriculo_Identity on ACA_Curriculo;
+insert into ACA_Curriculo
+       (cur_id, crr_id, crr_codigo, crr_regimeMatricula, crr_periodosNormal, crr_diasLetivos,
+        crr_vigenciaInicio, crr_situacao, crr_dataCriacao, crr_dataAlteracao)
+select cur_id + 20 as cur_id, crr_id, crr_codigo, crr_regimeMatricula, crr_periodosNormal, crr_diasLetivos,
+       crr_vigenciaInicio, crr_situacao, GETDATE(), GETDATE()
+  from ACA_Curriculo
+ where cur_id in (select cur_id from ACA_Curso where cur_nome like '2018%')
+
+if @@ERROR <> 0
+begin
+   PRINT 'Erro na criacao de curriculos'
+   ROLLBACK
+   RETURN
+end;
+ENABLE TRIGGER TRG_ACA_Curriculo_Identity on ACA_Curriculo;
+
+DISABLE TRIGGER TRG_ACA_CurriculoPeriodo_Identity on ACA_CurriculoPeriodo;
+
+INSERT INTO ACA_CurriculoPeriodo
+       (cur_id, crr_id, crp_id, crp_ordem, crp_descricao, crp_controleTempo, crp_qtdeDiasSemana,
+        crp_qtdeTemposDia, crp_qtdeTemposSemana, crp_qtdeHorasDia, crp_qtdeMinutosDia,
+        crp_qtdeEletivasAlunos, crp_ciclo, crp_turmaAvaliacao, crp_concluiNivelEnsino,
+        crp_situacao, crp_dataCriacao, crp_dataAlteracao, tci_id, tcp_id)
+select cur_id + 20, crr_id, crp_id, crp_ordem, crp_descricao, crp_controleTempo, crp_qtdeDiasSemana, crp_qtdeTemposDia, 
+	   crp_qtdeTemposSemana, crp_qtdeHorasDia, crp_qtdeMinutosDia, crp_qtdeEletivasAlunos, crp_ciclo, crp_turmaAvaliacao, 
+	   crp_concluiNivelEnsino, crp_situacao, GETDATE(), GETDATE(), tci_id, tcp_id
+  from ACA_CurriculoPeriodo
+ where cur_id in (select cur_id from ACA_Curso where cur_nome like '2018%')
+ 
+if @@ERROR <> 0
+begin
+   PRINT 'Erro na criacao de periodos para os novos curriculos'
+   ROLLBACK
+   RETURN
+end;
+ENABLE TRIGGER TRG_ACA_CurriculoPeriodo_Identity on ACA_CurriculoPeriodo;
+
+--drop table #TB_DISCIPL
+CREATE TABLE #TB_DISCIPL (dis_id int NULL, dis_id_novo int NULL, tds_id int, 
+dis_nome varchar(50), dis_nomeAbreviado varchar(10), dis_codigo varchar(5), 
+crd_tipo tinyint, cur_id int, crp_id int, dis_cargaHorariaTeorica int)
+
+insert into #TB_DISCIPL (dis_id, dis_nome, dis_nomeAbreviado, tds_id, dis_codigo, crd_tipo, cur_id, crp_id, dis_cargaHorariaTeorica) 
+select dis.dis_id, dis_nome, dis_nomeAbreviado, tds_id, dis_codigo, crd_tipo, cur_id + 20, crp_id, dis_cargaHorariaTeorica
+  from ACA_Disciplina dis
+	   inner join ACA_CurriculoDisciplina crd on crd.dis_id = dis.dis_id 
+											 and crd.cur_id in (select cur_id from ACA_Curso where cur_nome like '2018%')
+											 and crd.crd_situacao <> 3
+ where dis.dis_situacao <> 3
+   and dis.dis_id in (select dis_id from ACA_CurriculoDisciplina where cur_id = crd.cur_id and crd_situacao <> 3)
+ 
+DECLARE @dis_id_max int
+select @dis_id_max = MAX(dis_id) from ACA_Disciplina
+update #TB_DISCIPL set @dis_id_max = dis_id_novo = @dis_id_max + 1
+
+--select * from #TB_DISCIPL order by cur_id, dis_id
+
+SET IDENTITY_INSERT ACA_Disciplina ON 
+insert into ACA_Disciplina
+       (dis_id, tds_id, dis_codigo, dis_nome, dis_nomeAbreviado, dis_cargaHorariaTeorica,
+        dis_situacao, dis_dataCriacao, dis_dataAlteracao)
+select dis_id_novo, tds_id, dis_codigo, dis_nome, dis_nomeAbreviado, dis_cargaHorariaTeorica, 1, GETDATE(), GETDATE()
+  from #TB_DISCIPL 
+
+if @@ERROR <> 0
+begin
+   PRINT 'Erro na criacao de disciplinas'
+   ROLLBACK
+   SET IDENTITY_INSERT ACA_Disciplina OFF
+   RETURN
+end;
+
+SET IDENTITY_INSERT ACA_Disciplina OFF
+
+insert into ACA_CurriculoDisciplina
+       (cur_id, crr_id, crp_id, dis_id, crd_tipo, crd_situacao, crd_dataCriacao, crd_dataAlteracao)
+select cur_id, 1 as crr_id, crp_id, dis_id_novo, crd_tipo, 1 as crd_situacao, GETDATE(), GETDATE()
+  from #TB_DISCIPL
+ order by cur_id, dis_id
+
+if @@ERROR <> 0
+begin
+   PRINT 'Erro na criacao de disciplinas para os novos curriculos'
+   ROLLBACK
+   RETURN
+end;
+
+update ACA_CurriculoDisciplina set crd_situacao = 3, crd_dataAlteracao = GETDATE()
+  from ACA_CurriculoDisciplina
+	   inner join ACA_Disciplina dis on dis.dis_id = ACA_CurriculoDisciplina.dis_id and dis.dis_situacao = 3
+ where crd_situacao = 1
+ 
+--select cur_id, COUNT(*) from ACA_CurriculoDisciplina where crd_situacao = 1 group by cur_id order by 1
+--select * from Manutencao..DEPARA_CURSOS_DIVIDIDOS order by cur_id
+--select * from ACA_Curso where cur_id in (select cur_id from Manutencao..DEPARA_CURSOS_DIVIDIDOS)
+
+update Manutencao..DEPARA_CURSOS_DIVIDIDOS set cur_id = cur_id + 20 where cur_id in (select cur_id from ACA_Curso where cur_nome like '2018%')
+
+COMMIT
+
+
---
+++ scripts_virada_do_ano_2018_2019/RODAR APÓS a Integracao - Cadastra na ACA_CurriculoEscola e Periodo.sql
@@ -0,0 +1,57 @@
+USE GestaoPedagogica
+GO
+
+UPDATE ACA_CurriculoEscola set ces_vigenciaFim = '2018-12-31' where ces_vigenciaFim is null and ces_situacao <> 3
+
+insert into ACA_CurriculoEscola
+select distinct cur_id, crr_id, tur.esc_id, uni_id, 1 as ces_id, '2019-01-01' as ces_vigenciaInicio,
+       cast(null as date) as ces_vigenciafim, 1 as ces_situacao, GETDATE() as ces_DataCriacao,
+       GETDATE() as ces_DataAlteracao, 1 as vis_id
+  from TUR_Turma tur
+       inner join TUR_TurmaCurriculo tcr
+       on tur.tur_id = tcr.tur_id and tcr.tcr_situacao <> 3
+       inner join ESC_Escola esc
+       on tur.esc_id = esc.esc_id
+       inner join ACA_CalendarioAnual cal
+       on tur.cal_id = cal.cal_id
+ where tur.tur_situacao <> 3
+   and tcr.tcr_situacao <> 3
+   and esc.esc_situacao <> 3
+   and cal.cal_situacao <> 3
+   and cal.cal_ano = 2019
+   and cur_id not in (select cur_id from ACA_CurriculoEscola ces
+                       where ces.cur_id = tcr.cur_id
+                         and ces.crr_id = tcr.crr_id
+                         and ces.esc_id = tur.esc_id
+                         and ces.uni_id = tur.uni_id)
+ 
+ insert into ACA_CurriculoEscolaPeriodo
+select distinct ces.cur_id, ces.crr_id, ces.esc_id, ces.uni_id, ces.ces_id, tcr.crp_id, ces_situacao
+  from TUR_Turma tur
+       inner join TUR_TurmaCurriculo tcr
+       on tur.tur_id = tcr.tur_id and tcr.tcr_situacao <> 3
+       inner join ESC_Escola esc
+       on tur.esc_id = esc.esc_id
+       inner join ACA_CalendarioAnual cal
+       on tur.cal_id = cal.cal_id
+       inner join ACA_CurriculoEscola  ces
+        on tcr.cur_id = ces.cur_id
+       and tcr.crr_id = ces.crr_id
+       and tur.esc_id = ces.esc_id
+       and tur.uni_id = ces.uni_id
+ where tur.tur_situacao <> 3 
+   and tcr.tcr_situacao <> 3
+   and esc.esc_situacao <> 3
+   and cal.cal_situacao <> 3
+   and cal.cal_ano = 2019
+   and ces.ces_situacao <> 3
+   and ces.cur_id not in (select cur_id from ACA_CurriculoEscolaPeriodo cep
+                           where cep.cur_id = ces.cur_id
+                             and cep.crr_id = ces.crr_id
+                             and cep.esc_id = ces.esc_id
+                             and cep.uni_id = ces.uni_id
+                             and cep.ces_id = ces.ces_id
+                             and cep.crp_id = tcr.crp_id)
+
+--update ACA_CurriculoEscola set ces_vigenciaFim = '2016-12-31' 
+-- where ces_vigenciaInicio < '2017-01-01' and ces_situacao = 1 and ces_vigenciaFim is null
